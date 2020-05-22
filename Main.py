c1ad6d33bae09cda56a5a4ae6992bdeec64a7a1f
Avijit kanti Das <avijitnsec@gmail.com>
2020-05-22 14:44:35 +0530

    Adding dependency list file and connect to firebase.

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
 print ("Stocker server")
 
+from firebase import firebase
+
+fb = firebase.FirebaseApplication("https://fir-demo-9cfba.firebaseio.com/", None)
+
+data = {
+    'Name' : 'Avijit Das',
+    'email' : 'avijitnsec@gmail.com'
+
+}
+
+result = fb.post('/fir-demo-9cfba/student', data)
+
+print(result)
